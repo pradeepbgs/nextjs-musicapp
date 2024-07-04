340a5348275c64c89e078921e99698a36eb70651
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-07-04 13:32:28 +0530

    made courses section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedCourses from '@/components/FeaturedCourses';
 import HeroSection from '@/components/HeroSection'
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[]">
       <HeroSection/>
+      <FeaturedCourses/>
     </div>
   );
 }
